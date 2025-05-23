f586da239427562194d0ac841063295245d4a10d
JP Connolly <jp.connolly@vuori.com>
2025-05-23 12:38:35 -0700

    adding test api routes

---
+++ pages/api/created.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type ResponseData = {
+  message: string
+}
+ 
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  if (!req.headers['x-netlify-event'] || req.headers['x-netlify-event'] !== 'deploy_created') {
+    return {
+        status: 400,
+        body: "Invalid event type.",
+    }
+  }
+
+  try {
+    res.status(200).json({ message: 'Successfully Triggered Action from DEPLOY_CREATED' })
+  } catch (e) {
+    res.status(500).json({ message: `ERROR: ${e}`})
+  }
+}
---
+++ pages/api/restore.ts
@@ -0,0 +1,52 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type ResponseData = {
+  message: string
+}
+ 
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  if (!req.headers['x-netlify-event'] || req.headers['x-netlify-event'] !== 'deploy_restored') {
+    return {
+        status: 400,
+        body: "Invalid event type.",
+    }
+  }
+
+  try {
+    // const body = req.body
+    // const nameParts = body.name.split('-');
+    // const payload = JSON.stringify({
+    //     ref: body.branch,
+    //     inputs: {
+    //         'state': body.state,
+    //         'deploy_url': body.deploy_url,
+    //         'context': body.context,
+    //         'environment': nameParts[1],
+    //         'country': nameParts[2],
+    //     },
+    // });
+
+    // const response = await fetch(ghUrl, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Authorization': `Bearer ${token}`,
+    //     'Accept' : 'vnd.github+json',
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({
+    //     'event_type': 'webhook-trigger',
+    //     'client_payload': {
+    //       release: 'TEST FILE'
+    //     }
+    //   }),
+    // })
+
+    console.log('RESPONSE: ' )
+    res.status(200).json({ message: 'Successfully Triggered Action from DEPLOY_RESTORED' })
+  } catch (e) {
+    res.status(500).json({ message: `ERROR: ${e}`})
+  }
+}
